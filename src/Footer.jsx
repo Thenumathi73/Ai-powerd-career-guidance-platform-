62b1791fc328fcf4ce8c72a7ba201ca6bca92158
Thenumathi73 <thenumathianuradha@gmail.com>
2026-06-18 17:04:46 +0530

    Create Footer.jsx

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white text-center p-4 mt-10">
+      © 2026 AI Career Guidance Platform
+    </footer>
+  );
+}
+
+export default Footer;
